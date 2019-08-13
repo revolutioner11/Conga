2191683c033641870944010672c0ef316b1c5b2b
Nikola Nikolov <revolutioner11@gmail.com>
2019-08-13 10:10:01 +0300

    add

---
+++ Conga/DoublyLinkedList.cpp
@@ -0,0 +1,46 @@
+#include "DoublyLinkedList.h"
+
+template<class T>
+inline DoublyLinkedList<T>::DoublyLinkedList()
+{
+
+}
+
+template<class T>
+DoublyLinkedList<T>::DoublyLinkedList(DoublyLinkedList const& OtherDLL)
+{
+}
+
+template<class T>
+DoublyLinkedList& DoublyLinkedList<T>::operator=(const DoublyLinkedList& OtherDLL)
+{
+	// TODO: insert return statement here
+}
+
+template<class T>
+DoublyLinkedList<T>::~DoublyLinkedList()
+{
+}
+
+template<class T>
+void DoublyLinkedList<T>::Merge(DoublyLinkedList const& OtherDLL)
+{
+	return false;
+}
+
+template<class T>
+void DoublyLinkedList<T>::Append(const T& Data)
+{
+}
+
+template<class T>
+T& DoublyLinkedList<T>::RemoveFirst()
+{
+	// TODO: insert return statement here
+}
+
+template<class T>
+T& DoublyLinkedList<T>::RemoveLast()
+{
+	// TODO: insert return statement here
+}
