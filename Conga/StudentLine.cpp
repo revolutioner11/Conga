24d54700f2533e8c720aa91ad70a8dd6d50f4745
Nikola Nikolov <revolutioner11@gmail.com>
2019-08-13 10:41:14 +0300

    add 2

--- Conga/StudentLine.cpp
+++ Conga/StudentLine.cpp
@@ -1,12 +1,51 @@
 #include "StudentLine.h"
 
 
+DoublyLinkedList<T>::DoublyLinkedList()
+{
 
-StudentLine::StudentLine()
+}
+
+template<class T>
+DoublyLinkedList<T>::DoublyLinkedList(DoublyLinkedList const& OtherDLL)
 {
 }
 
+template<class T>
+DoublyLinkedList& DoublyLinkedList<T>::operator=(const DoublyLinkedList& OtherDLL)
+{
+	// TODO: insert return statement here
+}
 
-StudentLine::~StudentLine()
+template<class T>
+DoublyLinkedList<T>::~DoublyLinkedList()
+{
+}
+
+template<class T>
+void DoublyLinkedList<T>::Merge(DoublyLinkedList const& OtherDLL)
+{
+	return false;
+}
+
+template<class T>
+void DoublyLinkedList<T>::AddEnd(const T& Data)
+{
+}
+
+template<class T>
+void DoublyLinkedList<T>::RemoveFirst()
+{
+	// TODO: insert return statement here
+}
+
+template<class T>
+void DoublyLinkedList<T>::RemoveLast()
+{
+	// TODO: insert return statement here
+}
+
+template<class T>
+void DoublyLinkedList<T>::RemoveAt()
 {
 }
--- Conga/StudentLine.h
+++ Conga/StudentLine.h
@@ -1,14 +1,44 @@
 #pragma once
 
+#include <string>
+
 class StudentLine
 {
 public:
-	StudentLine();
-	~StudentLine();
+	struct Node
+	{
+	public:
+		string
+		Node* pPrev;
+		Node* pNext;
+
+		Node(const T& Data, Node* pPrev = nullptr, Node* pNext = nullptr)
+			: Data(Data), pPrev(pPrev), pNext(pNext)
+		{
+		}
+	};
+
+public:
+	DoublyLinkedList();
+	DoublyLinkedList(DoublyLinkedList const& OtherDLL);
+	DoublyLinkedList& operator=(const DoublyLinkedList& OtherDLL);
+	~DoublyLinkedList();
+
+	void Merge(DoublyLinkedList const& OtherDLL);
+	//void AddFront(const T& Data);
+	void AddEnd(const T& Data);				// This will be our APPEND
+	void RemoveFirst();
+	void RemoveLast();
+	void RemoveAt();
 
 
 
 private:
+	void Free()
 
+private:
+	Node* pFirst;
+	Node* pLast;
+	size_t Length;
 };
 
